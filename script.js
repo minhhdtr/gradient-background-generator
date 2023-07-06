14a5de22ae0006c213dcc27cea3bef56d8c76c66
duc minhh <ducminhh219@gmail.com>
2023-07-06 22:49:27 +0700

    separate 2 random button

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ $(document).on("click", function (event) {
 // toggle light/dark mode
 $(".btn.btn-toggle").on("click", () => {
   if (document.documentElement.getAttribute("data-bs-theme") === "dark") {
+    $(".btn-toggle i").removeClass("fa-sun").addClass("fa-moon");
     document.documentElement.setAttribute("data-bs-theme", "light");
   } else {
+    $(".btn-toggle i").removeClass("fa-moon").addClass("fa-sun");
     document.documentElement.setAttribute("data-bs-theme", "dark");
   }
 });
@@ -17,7 +19,7 @@ $(".btn.btn-toggle").on("click", () => {
 let color1 = $(".color-1"),
   color2 = $(".color-2");
 
-const directions = ["to top", "to right", "to bottom", "to left"];
+const directions = ["to bottom", "to top", "to left", "to right"];
 
 // generate random colors
 const randomColor = () => {
@@ -25,27 +27,41 @@ const randomColor = () => {
   return `#${color}`;
 };
 
-// set gradient background and change css code content
-const setGradientBackground = () => {
-  $(".gradient-background").css(
-    "background",
-    `linear-gradient(${color1.val()}, ${color2.val()})`
-  );
-  $(".bg-code").text(
-    `background: linear-gradient(to-bottom, ${color1.val()}, ${color2.val()});`
+// change css code content
+const setCssCode = (color1, color2, direction) => {
+  $(".code-preview").text(
+    `background: linear-gradient(${direction}, ${color1}, ${color2});`
   );
 };
 
+// set gradient background and change css code content
+const setGradientBackground = (color1, color2, direction) => {
+  $(".gradient-background").css(
+    "background",
+    `linear-gradient(${direction}, ${color1}, ${color2})`
+  );
+  $(".color-code-1 pre code").text(color1);
+  $(".color-code-2 pre code").text(color2);
+};
+
 color1.on("input", () => {
-  setGradientBackground();
+  setGradientBackground(color1.val(), color2.val(), directions[0]);
+  setCssCode(color1.val(), color2.val(), directions[0]);
 });
 
 color2.on("input", () => {
-  setGradientBackground();
+  setGradientBackground(color1.val(), color2.val(), directions[0]);
+  setCssCode(color1.val(), color2.val(), directions[0]);
 });
 
-$(".btn-random").on("click", () => {
+$("#btn-random-1").on("click", () => {
   color1.val(randomColor());
+  setGradientBackground(color1.val(), color2.val(), directions[0]);
+  setCssCode(color1.val(), color2.val(), directions[0]);
+});
+
+$("#btn-random-2").on("click", () => {
   color2.val(randomColor());
-  setGradientBackground();
+  setGradientBackground(color1.val(), color2.val(), directions[0]);
+  setCssCode(color1.val(), color2.val(), directions[0]);
 });
